9ee5c580f8f58ec56514405a58c79758c872160c
David Larsson <guslardaac@student.gu.se>
2017-03-27 15:38:57 +0200

    Test push to communication branch

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/communication/ConnectActivity.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/communication/ConnectActivity.java
@@ -1,7 +1,10 @@
 package group8.scam.model.communication;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import group8.scam.R;
 
@@ -11,5 +14,8 @@ public class ConnectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+    Button btnPair;
+    ListView deviceList;
 }
