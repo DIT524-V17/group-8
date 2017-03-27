a6f8fcb2e3de2e9dbd7ce9c2019ddd3ca2098494
Samuel <sbackstrom95@gmail.com>
2017-03-27 15:04:14 +0200

    Created Basic Structure of the App

---
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/control/JoystickActivity.java
@@ -0,0 +1,15 @@
+package group8.scam.model.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group8.scam.R;
+
+public class JoystickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joystick);
+    }
+}
--- RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/RadarActivity.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/RadarActivity.java
@@ -1,13 +1,15 @@
-package group8.scam;
+package group8.scam.model.radar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ConnectActivity extends AppCompatActivity {
+import group8.scam.R;
+
+public class RadarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connect);
+        setContentView(R.layout.activity_radar);
     }
 }
