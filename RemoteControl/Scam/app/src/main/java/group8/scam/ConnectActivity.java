6bc3231a2a82fc5d6506431eb4d6a8c6b37b8ed0
Samuel <sbackstrom95@gmail.com>
2017-03-27 13:35:19 +0200

    Re-do

---
+++ RemoteControl/Scam/app/src/main/java/group8/scam/ConnectActivity.java
@@ -0,0 +1,13 @@
+package group8.scam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+    }
+}
