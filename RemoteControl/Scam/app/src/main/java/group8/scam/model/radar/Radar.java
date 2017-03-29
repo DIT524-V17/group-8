8e99c2e88b1ff6912a1553dd41738946121cfcdf
firsou <guschefi@student.gu.se>
2017-03-29 11:39:37 +0200

    Added setters and getters to Radar class, also private variables init

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/Radar.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/Radar.java
@@ -1,4 +1,28 @@
 package group8.scam.model.radar;
 
 public class Radar {
+
+    private int angleOfServo = 0;
+    private int ultrasonicReading = 0;
+
+    public Radar (int angleOfServo, int ultrasonicReading) {
+        this.angleOfServo = angleOfServo;
+        this.ultrasonicReading = ultrasonicReading;
+    }
+
+    /**
+     * Modifies the current Servo angle
+     * @param newAngle
+     */
+    public void setAngle(int newAngle) {
+        this.angleOfServo = newAngle;
+    }
+
+    /**
+     * Retrieves the current Servo Angle
+     * @return
+     */
+    public int getAngle() {
+        return this.angleOfServo;
+    }
 }
