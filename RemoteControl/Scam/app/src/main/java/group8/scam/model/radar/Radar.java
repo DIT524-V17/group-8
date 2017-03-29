202e379035b2db0c9e963a77ff9f2b3ecb77a8ca
firsou <guschefi@student.gu.se>
2017-03-29 12:27:53 +0200

    Added radarbackground shape, new color, defined min and Max angle on radar

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/Radar.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/radar/Radar.java
@@ -4,15 +4,17 @@ public class Radar {
 
     private int angleOfServo = 0;
     private int ultrasonicReading = 0;
+    private int minAngle = 0;
+    private int maxAngle = 0;
 
-    public Radar (int angleOfServo, int ultrasonicReading) {
-        this.angleOfServo = angleOfServo;
-        this.ultrasonicReading = ultrasonicReading;
+    public Radar (int minAngle, int maxAngle) {
+        this.minAngle = minAngle;
+        this.maxAngle = maxAngle;
     }
 
     /**
      * Modifies the current Servo angle
-     * @param newAngle
+     * @param newAngle the new angle the Servo is currently at
      */
     public void setAngle(int newAngle) {
         this.angleOfServo = newAngle;
@@ -20,9 +22,25 @@ public class Radar {
 
     /**
      * Retrieves the current Servo Angle
-     * @return
+     * @return int angleOfServo
      */
     public int getAngle() {
         return this.angleOfServo;
+    }
+
+    /**
+     * Retrieves the minimum Angle of the Servo
+     * @return int minAngle
+     */
+    public int getMinAngle() {
+        return this.minAngle;
+    }
+
+    /**
+     * Retrieves the maximum angle of the Servo
+     * @return int maxAngle
+     */
+    public int getMaxAngle() {
+        return this.maxAngle;
     }
 }
