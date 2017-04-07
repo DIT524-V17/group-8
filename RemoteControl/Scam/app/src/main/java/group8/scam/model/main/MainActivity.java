fd92747fcf9afe1757506b315089ef2870a405fc
firsou <guschefi@student.gu.se>
2017-04-07 13:49:15 +0200

    Set background color in MainActivity

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
@@ -1,5 +1,6 @@
 package group8.scam.model.main;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.mainColor));
+        getWindow().getDecorView().setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.mainColor));
 
     }
 }
