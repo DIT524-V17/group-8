d50e0deeecc9f1a92dcb1533afff58c7e3f4d6f4
firsou <guschefi@student.gu.se>
2017-04-07 13:27:12 +0200

    Added positioning, color and text labels

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
@@ -7,9 +7,12 @@ import group8.scam.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.mainColor));
+
     }
 }
