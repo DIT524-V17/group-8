58247e876c4ffda2ab6565357247391c3edeef9b
firsou <guschefi@student.gu.se>
2017-04-07 10:04:08 +0200

    Created new class RadarView

---
+++ RemoteControl/Scam/app/src/main/java/group8/scam/view/RadarView.java
@@ -0,0 +1,25 @@
+package group8.scam.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Firsou on 2017-04-07.
+ */
+
+public class RadarView extends View {
+
+    public RadarView(Context context) {
+        super(context);
+    }
+
+    public RadarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
