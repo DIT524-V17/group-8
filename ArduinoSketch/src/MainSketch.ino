7fe0c1f8a0f842bbc956bc343998dcfe36167eaa
N0ah_ <gusfawom@student.gu.se>
2017-04-21 17:26:47 +0200

    Sprint 5 car version

--- ArduinoSketch/src/MainSketch.ino
+++ ArduinoSketch/src/MainSketch.ino
@@ -54,6 +54,9 @@ int averageServo = 0;
 
 String selectStr;
 String data;
+bool isBeginning = true;
+bool isAuto = false;
+bool isManual = false;
 
 char terminator = ':';
 
@@ -90,7 +93,6 @@ void setup() {
 void loop() {
   if (Serial3.available()) {
     selectStr = Serial3.readString();
-    Serial.println("HELLO");
     if (selectStr.equals("m")) {
       manualDrive();
     }
@@ -128,35 +130,35 @@ void manualDrive() {
 }
 
 void autoDrive() {
-  car.setSpeed(25);
+  car.setSpeed(35);
+  delay(100);
   while (true) {
+    car.setSpeed(25);
     getLeftReading();
     getRightReading();
     getServoReading();
     int staticDist = centerSonic.getDistance();
+    Serial3.println(staticDist);
     if (staticDist < 45 && staticDist != 0) {
       if (averageLeft < 35 || averageRight < 35)  {
         if (averageLeft < averageRight) {
           car.rotate(40);
-          delay(200);
+          delay(100);
         }
         else if (averageLeft > averageRight) {
           car.rotate(-40);
-          delay(200);
+          delay(100);
         }
-      } /* else if (staticDist < 25) {
-        car.rotate(35);
+      }
+      else if (staticDist < 10 && staticDist != 0) {
+        car.setSpeed(-30);
+        delay(500);
+        car.stop();
+        delay(200);
+        rotateOnSpot(90);
         delay(200);
       }
-      */
-    } /* else if (averageLeft < 10 && staticDist < 10) {
-      car.rotate(15);
-      delay(200);
-    } else if (averageRight < 10 && staticDist < 10) {
-      car.rotate(-15);
-      delay(200);
     }
-    */
   }
 }
 
@@ -165,7 +167,7 @@ void getLeftReading() {
   leftReadings[readIndexLeft] = leftSonic.getDistance();
 
   if (leftReadings[readIndexLeft] == 0) {
-    leftReadings[readIndexLeft] = 40;
+    leftReadings[readIndexLeft] = 35;
   }
 
   totalLeft = totalLeft + leftReadings[readIndexLeft];
@@ -184,7 +186,7 @@ void getRightReading() {
   rightReadings[readIndexRight] = rightSonic.getDistance();
 
   if (rightReadings[readIndexRight] == 0) {
-    rightReadings[readIndexRight] = 40;
+    rightReadings[readIndexRight] = 35;
   }
 
   totalRight = totalRight + rightReadings[readIndexRight];
@@ -203,7 +205,7 @@ void getCenterReading() {
   centerReadings[readIndexCenter] = centerSonic.getDistance();
 
   if (centerReadings[readIndexCenter] == 0) {
-    centerReadings[readIndexCenter] = 30;
+    centerReadings[readIndexCenter] = 40;
   }
 
   totalCenter = totalCenter + centerReadings[readIndexCenter];
@@ -234,4 +236,34 @@ void getServoReading() {
 
   averageServo = totalServo / numReadingsServo;
   delay(1);
+}
+
+/*
+This method was taken from the examples presented by Dimitris Platis
+*/
+void rotateOnSpot(int targetDegrees) {
+  targetDegrees %= 360; //put it on a (-360,360) scale
+  if (!targetDegrees) return; //if the target degrees is 0, don't bother doing anything
+  /* Let's set opposite speed on each side of the car, so it rotates on spot */
+  if (targetDegrees > 0) { //positive value means we should rotate clockwise
+    car.setMotorSpeed(motorSpeed, -motorSpeed); // left motors spin forward, right motors spin backward
+  } else { //rotate counter clockwise
+    car.setMotorSpeed(-motorSpeed, motorSpeed); // left motors spin backward, right motors spin forward
+  }
+  unsigned int initialHeading = gyro.getAngularDisplacement(); //the initial heading we'll use as offset to calculate the absolute displacement
+  int degreesTurnedSoFar = 0; //this variable will hold the absolute displacement from the beginning of the rotation
+  while (abs(degreesTurnedSoFar) < abs(targetDegrees)) { //while absolute displacement hasn't reached the (absolute) target, keep turning
+    gyro.update(); //update to integrate the latest heading sensor readings
+    int currentHeading = gyro.getAngularDisplacement(); //in the scale of 0 to 360
+    if ((targetDegrees < 0) && (currentHeading > initialHeading)) { //if we are turning left and the current heading is larger than the
+      //initial one (e.g. started at 10 degrees and now we are at 350), we need to substract 360, so to eventually get a signed
+      currentHeading -= 360; //displacement from the initial heading (-20)
+    } else if ((targetDegrees > 0) && (currentHeading < initialHeading)) { //if we are turning right and the heading is smaller than the
+      //initial one (e.g. started at 350 degrees and now we are at 20), so to get a signed displacement (+30)
+      currentHeading += 360;
+    }
+    degreesTurnedSoFar = initialHeading - currentHeading; //degrees turned so far is initial heading minus current (initial heading
+    //is at least 0 and at most 360. To handle the "edge" cases we substracted or added 360 to currentHeading)
+  }
+  car.stop(); //we have reached the target, so stop the car
 }
