b0a6a6b818b263d49ca809d3106e0d219db668f4
firsou <guschefi@student.gu.se>
2017-04-26 14:42:15 +0200

    Created filter method to handle ultrasonic distance reading - in Radar Model class

--- RemoteControl/scam/app/src/main/java/group8/scam/model/radar/RadarData.java
+++ RemoteControl/scam/app/src/main/java/group8/scam/model/radar/RadarData.java
@@ -6,6 +6,7 @@ public class RadarData {
     private int ultrasonicReading;
     private int minAngle = 0;
     private int maxAngle = 0;
+    private String servoString;
 
     public RadarData(int minAngle, int maxAngle) {
         this.minAngle = minAngle;
@@ -53,9 +54,30 @@ public class RadarData {
     }
 
     /**
-     * Retrieves ultrasonic reading
-     * @return int ultrasonic reading
+     * This method retrieves the String sent by the car to the Handler and
+     * processes the string by verifying whether a character is a number/digit
+     * or not. This ensures that any text indicators and unwanted data are filtered
+     * and only the distance reading remains.
+     * <p>
+     * Furthermore, the string used to retrieve the distance reading is emptied
+     * after filtering to ensure future calls of the method are correct.
+     *
+     * @param servoData - Raw data received as a string from the Handler
+     * @return the distance reading from the ultrasonic sensor
      */
+    public int filterUltrasonicReading(String servoData) {
+
+        for (int i = 0; i < servoData.length(); i++) {
+            if (Character.isDigit(servoData.charAt(i))) {
+                servoString += servoData.charAt(i);
+            }
+        }
+
+        servoString = "";
+        this.ultrasonicReading = Integer.parseInt(servoString);
+        return getUltrasonicReading();
+    }
+
     public int getUltrasonicReading() {
         return this.ultrasonicReading;
     }
