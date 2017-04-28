5a34fafa159ab7d4cedac7d550052d355025f49e
Markus Wrang <Markus Wrang>
2017-04-28 15:31:25 +0200

    added logic for the buttons in the dpad

--- RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
+++ RemoteControl/Scam/app/src/main/java/group8/scam/model/main/MainActivity.java
@@ -2,9 +2,11 @@ package group8.scam.model.main;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -20,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ToggleButton button;
     private String stateString;
+    private String dataStr;
     private HandleThread handler = HandleThread.getInstance();
+    private Button btnleft, btnright, btnup, btndown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         button = (ToggleButton) findViewById(R.id.togglebutton);
+
+        btnleft = (Button)findViewById(R.id.btnleft);
+        btnright = (Button)findViewById(R.id.btnRight);
+        btnup = (Button)findViewById(R.id.btnUp);
+        btndown = (Button)findViewById(R.id.btnDown);
+
         button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                 /*
@@ -44,6 +54,103 @@ public class MainActivity extends AppCompatActivity {
                 msg.sendToTarget();
             }
         });
+
+        /*
+        *logic for the left button in dpad
+        *
+         */
+        btnleft.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    dataStr = "-50:50:";
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    dataStr = " STOP";
+                }
+                Message msg = handler.getHandler().obtainMessage();
+                msg.what = MESSAGE_WRITE;
+                msg.obj = dataStr;
+                msg.sendToTarget();
+
+                return true;
+            }
+        });
+
+        /*
+        *logic for the dow button in dpad
+        *
+         */
+        btndown.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    dataStr = "0:-50:";
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    dataStr = " STOP";
+                }
+                Message msg = handler.getHandler().obtainMessage();
+                msg.what = MESSAGE_WRITE;
+                msg.obj = dataStr;
+                msg.sendToTarget();
+
+                return true;
+            }
+        });
+
+        /*
+        *logic for the right button in dpad
+        *
+         */
+        btnright.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    dataStr = "50:50:";
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    dataStr = " STOP";
+                }
+                Message msg = handler.getHandler().obtainMessage();
+                msg.what = MESSAGE_WRITE;
+                msg.obj = dataStr;
+                msg.sendToTarget();
+
+                return true;
+            }
+        });
+
+        /*
+        *logic for the up button in dpad
+        *
+         */
+        btnup.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    dataStr = "0:50:";
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    dataStr = " STOP";
+                }
+                Message msg = handler.getHandler().obtainMessage();
+                msg.what = MESSAGE_WRITE;
+                msg.obj = dataStr;
+                msg.sendToTarget();
+
+                return true;
+            }
+        });
+
     }
 
     public void btnSettings(View view) {
@@ -52,22 +159,5 @@ public class MainActivity extends AppCompatActivity {
         this.overridePendingTransition(0, 0);
     }
 
-
-    // The logic for the dpad buttons
-    public void btnUp(View view){
-
-    }
-
-    public void btnDown(View view){
-
-    }
-
-    public void btnRight(View view){
-
-    }
-
-    public void btnLeft(View view){
-
-    }
 
 }
